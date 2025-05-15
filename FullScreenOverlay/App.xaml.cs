4edb6a2363dd5e2474d056f2aa6bd4885e964e0f
DeTosis <alexnev1803STDota@yandex.ru>
2025-05-15 19:26:14 +0500

    some things done

--- FullScreenOverlay/App.xaml.cs
+++ FullScreenOverlay/App.xaml.cs
@@ -3,22 +3,31 @@ using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace FullScreenOverlay;
 
 public partial class App : Application {
-    private static Window mainWindow;
+    private static MainWindow mainWindow;
     private static bool isWindowActive;
 
     private static bool canBeClosed;
+    static bool ctrlPressed;
+    static bool shiftPressed;
+
     public static bool CanBeClosed {
         get { return canBeClosed; }
         set { canBeClosed = value; }
     }
 
-
     public void ApplicationStartup(object sender, StartupEventArgs e) {
+        var p = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+        if (p.Length > 1) {
+            Process.GetCurrentProcess().Kill();
+        }
+
         mainWindow = new MainWindow();
+        mainWindow.MainWindowLoaded += OnMainWindowLoaded;
 
         using (Process process = Process.GetCurrentProcess())
         using (ProcessModule module = process.MainModule) {
@@ -30,33 +39,66 @@ public partial class App : Application {
         }
     }
 
+    private void OnMainWindowLoaded(object? sender, nint e) {
+        HwndSource source = HwndSource.FromHwnd(e);
+        source.AddHook(WndProc);
+    }
+
+    // LL HOTKEY SUPPRESSING
+    private IntPtr WndProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled) {
+        if (msg == WM_KILLFOCUS && isWindowActive) {
+            //TODO: losse focus ony when other application is focused | currently: ineer windows can close overlay too
+            DeactivateWindow();
+        }
+
+        if (msg == WM_SYSKEYDOWN) {
+            if (wParam == VK_F4 && GetAsyncKeyState(VK_MENU) != 0) {
+                handled = true;
+                return IntPtr.Zero;
+            }
+        }
+
+        if (msg == WM_WINDOWPOSCHANGING) {
+            var wp = Marshal.PtrToStructure<WINDOWPOS>(lParam);
+            wp.flags |= SWP_NOMOVE | SWP_NOSIZE;
+            Marshal.StructureToPtr(wp, lParam, false);
+
+            handled = true;
+            return IntPtr.Zero;
+        }
+
+        if (msg == WM_CLOSE) {
+            DeactivateWindow();
+            handled = true;
+            return IntPtr.Zero;
+        }
+
+        return IntPtr.Zero;
+    }
+
     public static void UnHook() {
         UnhookWindowsHookEx(hHook);
     }
 
     public static void DeactivateWindow() {
-        mainWindow.Hide();
         isWindowActive = false;
+        mainWindow.Hide();
     }
 
-    static bool ctrlPressed;
-    static bool shiftPressed;
-    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+    private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
         if (nCode >= 0 && wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
             if (CanBeClosed) {
-                CanBeClosed = false;
                 DeactivateWindow();
-                return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
             }
 
             var skb = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
             if (skb.vkCode == VK_ESCAPE && isWindowActive) {
                 CanBeClosed = false;
+                isWindowActive = false;
                 DeactivateWindow();
-                return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
             }
 
-            //OpenUp
+            //Show Overlay
             if (!ctrlPressed)
                 ctrlPressed = skb.vkCode == VK_LCONTROL;
             if (!shiftPressed)
@@ -66,11 +108,12 @@ public partial class App : Application {
             if (ctrlPressed && shiftPressed && activationKeyPressed) {
                 if (!isWindowActive) {
                     isWindowActive = true;
-                    
+
                     mainWindow.Activate();
                     mainWindow.Show();
 
                     //CanBeClosed = true;
+                    return 1;
                 }
             }
         }
@@ -100,32 +143,54 @@ public partial class App : Application {
         IntPtr dwExtraInfo;
     }
 
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPOS {
+        public IntPtr hwnd;
+        public IntPtr hwndInsertAfter;
+        public int x;
+        public int y;
+        public int cx;
+        public int cy;
+        public int flags;
+    }
+
     // ***
 
     static IntPtr hHook;
-    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static LowLevelKeyboardProc _proc = KeyboardHookCallback;
     private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
     // ***
-    private const int VK_ESCAPE         = 0x1B;
 
-    private const int VK_Q              = 0x51;
+    private const int VK_ESCAPE = 0x1B;
 
-    private const int VK_LSHIFT         = 0xA0;
-    private const int VK_RSHIFT         = 0xA1;
-    private const int VK_LCONTROL       = 0xA2;
-    private const int VK_RCONTROL       = 0xA3;
+    private const int VK_Q = 0x51;
 
-    private const int WM_KEYDOWN        = 0x0100;
-    private const int WM_KEYUP          = 0x0101;
+    private const int VK_LSHIFT = 0xA0;
+    private const int VK_RSHIFT = 0xA1;
+    private const int VK_LCONTROL = 0xA2;
+    private const int VK_RCONTROL = 0xA3;
 
-    private const int WM_SYSKEYDOWN     = 0x0104;
-    private const int WM_SYSKEYUP       = 0x0105;
+    private const int WM_KEYDOWN = 0x0100;
+    private const int WM_KEYUP = 0x0101;
 
-    private const int WH_KEYBOARD_LL    = 13;
-    
+    private const int WM_SYSKEYDOWN = 0x0104;
+    private const int WM_SYSKEYUP = 0x0105;
+
+    private const int WH_KEYBOARD_LL = 13;
+
+    private const int VK_MENU = 0x12;
+    private const int VK_F4 = 0x73;
+
+    private const int WM_WINDOWPOSCHANGING = 0x0046;
+
+    private const int SWP_NOMOVE = 0x0002;
+    private const int SWP_NOSIZE = 0x0001;
+    private const int WM_CLOSE = 0x0010;
+    private const int WM_KILLFOCUS = 0x0008;
+
     // ***
-    
+
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn,
         IntPtr hMod, uint dwThreadId);
--- FullScreenOverlay/MVVM/View/MainWindow.xaml.cs
+++ FullScreenOverlay/MVVM/View/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
@@ -7,63 +6,15 @@ namespace FullScreenOverlay.MVVM.View;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
-
         Loaded += (s, e) => {
             var hwnd = new WindowInteropHelper(this).Handle;
-            HwndSource source = HwndSource.FromHwnd(hwnd);
-            source.AddHook(WndProc);
+            OnMainWindowLoaded(hwnd);
         };
     }
 
-    // LL HOTKEY SUPPRESSING
-    private IntPtr WndProc(nint hwnd, int msg, nint wParam, nint lParam, ref bool handled) {
-        if (msg == WM_SYSKEYDOWN) {
-            if (wParam == VK_F4 && GetKeyState(VK_MENU) != 0) {
-                handled = true;
-                return IntPtr.Zero;
-            }
-        }
-
-        if (msg == WM_WINDOWPOSCHANGING) {
-            var wp = Marshal.PtrToStructure<WINDOWPOS>(lParam);
-            wp.flags |= SWP_NOMOVE | SWP_NOSIZE;
-            Marshal.StructureToPtr(wp, lParam, false);
-            
-            handled = true;
-            return IntPtr.Zero;
-        }
-
-        if (msg == WM_CLOSE) {
-            App.DeactivateWindow();
-            handled = true;
-            return IntPtr.Zero;
-        }
-
-        return IntPtr.Zero;
+    public event EventHandler<nint> MainWindowLoaded;
+    private void OnMainWindowLoaded(nint hwnd) {
+        var handler = MainWindowLoaded;
+        handler?.Invoke(this, hwnd);
     }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct WINDOWPOS {
-        public IntPtr hwnd;
-        public IntPtr hwndInsertAfter;
-        public int x;
-        public int y;
-        public int cx;
-        public int cy;
-        public int flags;
-    }
-
-    private const int VK_MENU               = 0x12;
-    private const int VK_F4                 = 0x73;
-
-    private const int WM_SYSKEYDOWN         = 0x0104;
-    private const int WM_KEYDOWN            = 0x0100;
-    private const int WM_WINDOWPOSCHANGING  = 0x0046;
-
-    private const int SWP_NOMOVE            = 0x0002;
-    private const int SWP_NOSIZE            = 0x0001;
-    private const int WM_CLOSE              = 0x0010;
-
-    [DllImport("user32.dll")]
-    private static extern short GetKeyState(int nVirtKey);
 }
